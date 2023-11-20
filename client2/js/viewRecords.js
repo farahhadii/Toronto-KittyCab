5184aa1ac02137c216e6173f7f878d256fe2aaa2
Nikita Berezyuk ☭ <96497560+berezucc@users.noreply.github.com>
2023-11-19 21:40:47 -0500

    sdasdasd

--- client2/js/viewRecords.js
+++
@@ -1,8 +0,0 @@
-var params = new URLSearchParams(window.location.search);
-var table = params.get('table');
-
-if (table) {
-    document.getElementById('table-name').innerText = table;
-
-    // Okay we need to do the fetch request here
-}
