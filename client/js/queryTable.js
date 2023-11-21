93979509ddf318973efa5db057c23cdfa3c67f19
farah Adi <farahadi32@gmail.com>
2023-11-20 20:13:02 -0500

    add styling

--- client/js/queryTable.js
+++ client/js/queryTable.js
@@ -1,12 +1,3 @@
-async function myFunction() {
-    let tableName = 'Account';
-    const response = await fetch(`http://localhost:4000/create-table`);
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.splash-screen').style.display = 'flex';
     setTimeout(function() {
