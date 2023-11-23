399dd247b6df4c8a5819def0eaf21e262da7f716
Nikita Berezyuk ☭ <96497560+berezucc@users.noreply.github.com>
2023-11-22 20:33:52 -0500

    Create + Populate

--- server/backend.js
+++ server/backend.js
@@ -50,13 +50,6 @@ app.post('/create-table', async (req, res) => {
                 Experience INT,
                 FOREIGN KEY (AccountID) REFERENCES Account(AccountID)
             )`,
-            `CREATE TABLE Registered_Car (
-                CarID INT,
-                DriverID INT,
-                CarInsurance VARCHAR(255) CHECK (CarInsurance = 'Yes' OR CarInsurance = 'No'),
-                FOREIGN KEY (DriverID) REFERENCES Driver(DriverID),
-                PRIMARY KEY (CarID, DriverID)
-            )`,
             `CREATE TABLE Car_Model (
                 VINNumber INT CHECK (VINNumber >= 00000000000000001 and VINNumber <= 999999999999999999),
                 CarModelName VARCHAR(255),
@@ -67,9 +60,17 @@ app.post('/create-table', async (req, res) => {
                 CarID INT PRIMARY KEY,
                 CarMakeName VARCHAR(255),
                 CarModelName VARCHAR(255),
-                VINNumber INT CHECK (VINNumber >= 00000000000000001 and VINNumber <= 999999999999999999),
+                VINNumber INT,
                 CarTier VARCHAR(255),
                 FOREIGN KEY (VINNumber, CarModelName) REFERENCES Car_Model(VINNumber, CarModelName)
+            )`,
+            `CREATE TABLE Registered_Car (
+                CarID INT,
+                DriverID INT,
+                CarInsurance VARCHAR(255) CHECK (CarInsurance = 'Yes' OR CarInsurance = 'No'),
+                FOREIGN KEY (DriverID) REFERENCES Driver(DriverID),
+                FOREIGN KEY (CarID) REFERENCES Car_Make(CarID),
+                PRIMARY KEY (CarID, DriverID)
             )`,
             `CREATE TABLE Item (
                 ItemID INT PRIMARY KEY,
@@ -137,56 +138,69 @@ app.post('/populate-table', async (req, res) => {
         });
 
         const insertStatements = [
-            `INSERT INTO Account VALUES (1000, 'BillP5', 'Bill', 'Vier', 'BillVier@gmail.com', 'Bill1978', 1)`,
+            `INSERT INTO Account VALUES (1000, 'BillP5', 'Bill', 'Vier', 'BillVier@gmail.com', 'Bill19', 4)`,
             `INSERT INTO Account VALUES (1001, 'BobD5', 'Bob', 'Vier', 'BobVier@gmail.com', 'Bob1973', 4)`,
             `INSERT INTO Account VALUES (1002, 'JimD5', 'Jim', 'Vier', 'JimVier@gmail.com', 'Jim1923', 2)`,
-            `INSERT INTO Account VALUES (1003, 'JeffD3', 'Jeff', 'Vier', 'JeffVier@gmail.com', 'Jeff1223', 5)`,
-            `INSERT INTO Account VALUES (1004, 'Jeff43', 'Jeff', 'Smith', 'JeffSmith@gmail.com', 'Jeff3423', 3)`,
+            `INSERT INTO Account VALUES (1003, 'JeffD3', 'Jeff', 'Vier', 'JeffVier@gmail.com', 'Jeff12', 1)`,
+            `INSERT INTO Account VALUES (1004, 'Jeff43', 'Jeff', 'Smith', 'JeffSmith@gmail.com', 'Jeff', 2)`,
             `INSERT INTO Account VALUES (1005, 'Joe21', 'Joe', 'Doe', 'JoeDoe@gmail.com', 'Joe3', 2)`,
             `INSERT INTO Account VALUES (1006, 'Josh21', 'Josh', 'Doe', 'JoshDoe@gmail.com', 'Josh33', 4)`,
-            `INSERT INTO Account VALUES (1007, 'JakeP1', 'Jake', 'Paul', 'JakePaul@gmail.com', 'Jakey123', 3)`,
-            `INSERT INTO Account VALUES (1008, 'TomH3', 'Tom', 'Hardy', 'TomHardy@gmail.com', 'TomH_1977', 5)`,
-            `INSERT INTO Account VALUES (1009, 'AmyP2', 'Amy', 'Poehler', 'AmyPoehler@gmail.com', 'AmyFunny', 4)`,
-            `INSERT INTO Account VALUES (1010, 'SteveC4', 'Steve', 'Carell', 'SteveCarell@gmail.com', 'SteveOffice', 2)`,
+            `INSERT INTO Account VALUES (1007, 'JakeP1', 'Jake', 'Paul', 'JakePaul@gmail.com', 'Jakey1', 3)`,
+            `INSERT INTO Account VALUES (1008, 'TomH3', 'Tom', 'Hardy', 'TomHardy@gmail.com', 'TomH_19', 3)`,
+            `INSERT INTO Account VALUES (1009, 'AmyP2', 'Amy', 'Poehler', 'AmyPoehler@gmail.com', 'Amy', 5)`,
+            `INSERT INTO Account VALUES (1010, 'SteveC4', 'Steve', 'Carell', 'SteveCarell@gmail.com', 'Steve', 4)`,
+          
             `INSERT INTO Passenger VALUES (2000, 1000, 'premium', 30, '5')`,
             `INSERT INTO Passenger VALUES (2001, 1006, 'premium', 30, '5')`,
             `INSERT INTO Passenger VALUES (2002, 1007, 'basic', 10, '3')`,
             `INSERT INTO Passenger VALUES (2003, 1008, 'premium', 25, '8')`,
-            `INSERT INTO Car_Model VALUES (60000000000000024, 'camry', 2013)`,
-            `INSERT INTO Car_Model VALUES (60000000000000025, 'i3', 2015)`,
-            `INSERT INTO Car_Model VALUES (60000000000000026, 'i3', 2015)`,
-            `INSERT INTO Car_Model VALUES (60000000000000027, 'i3', 2014)`,
-            `INSERT INTO Car_Make VALUES (5000, 'toyota', 'camry', 60000000000000024, 'economy')`,
-            `INSERT INTO Car_Make VALUES (5001, 'BMW', 'i3', 60000000000000025, 'economy')`,
-            `INSERT INTO Car_Make VALUES (5002, 'BMW', 'i3', 60000000000000026, 'economy')`,
-            `INSERT INTO Car_Make VALUES (5003, 'BMW', 'i3', 60000000000000027, 'economy')`,
+          
             `INSERT INTO Driver VALUES (3000, 1001, 'U5143-32118-03671', 1)`,
             `INSERT INTO Driver VALUES (3001, 1002, 'U5123-75128-46672', 3)`,
             `INSERT INTO Driver VALUES (3002, 1003, 'G2114-55223-42672', 5)`,
             `INSERT INTO Driver VALUES (3003, 1004, 'R1234-56783-36782', 1)`,
             `INSERT INTO Driver VALUES (3004, 1009, 'T4345-67894-23678', 4)`,
             `INSERT INTO Driver VALUES (3005, 1010, 'L5678-43211-98567', 6)`,
+          
+            `INSERT INTO Car_Model VALUES (60000000000000024, 'camry', 2013)`,
+            `INSERT INTO Car_Model VALUES (60000000000000025, 'i3', 2015)`,
+            `INSERT INTO Car_Model VALUES (60000000000000026, 'a4', 2015)`,
+            `INSERT INTO Car_Model VALUES (60000000000000027, 'c400', 2014)`,
+          
+            `INSERT INTO Car_Make VALUES (4000, 'toyota', 'camry', 60000000000000024, 'economy')`,
+            `INSERT INTO Car_Make VALUES (4001, 'BMW', 'i3', 60000000000000025, 'platinum')`,
+            `INSERT INTO Car_Make VALUES (4002, 'audi', 'a4', 60000000000000026, 'green')`,
+            `INSERT INTO Car_Make VALUES (4003, 'mercedes', 'c400', 60000000000000027, 'platinum')`,
+          
+            `INSERT INTO Registered_Car VALUES (4000, 3000, 'Yes')`,
+            `INSERT INTO Registered_Car VALUES (4001, 3001, 'Yes')`,
+            `INSERT INTO Registered_Car VALUES (4002, 3002, 'Yes')`,
+            `INSERT INTO Registered_Car VALUES (4003, 3003, 'Yes')`,
+          
             `INSERT INTO Item VALUES (7001, 'Nike Shoe 2', 1.00, 'Nike', 'White running shoes')`,
             `INSERT INTO Item VALUES (7002, 'Adidas Jacket', 79.99, 'Adidas', 'Blue sports jacket')`,
-            `INSERT INTO Item VALUES (7003, 'Apple iPhone', 999.99, 'Apple Store', 'Latest iPhone model')`,
-            `INSERT INTO ORDERS VALUES (8000, '15/10/23', 5.00, 3000)`,
-            `INSERT INTO ORDERS VALUES (8001, '16/10/23', 8.00, 3001)`,
-            `INSERT INTO ORDERS VALUES (8002, '17/10/23', 50.00, 3002)`,
-            `INSERT INTO ORDERS VALUES (8003, '17/10/23', 99.00, 3003)`,
-            `INSERT INTO ORDERS VALUES (8004, '18/10/23', 15.00, 3004)`,
+            `INSERT INTO Item VALUES (7003, 'Apple iPhone', 999.99, 'Apple Store', 'Latest iPhone mode')`,
+          
+            `INSERT INTO ORDERS VALUES (8000, DATE '2015-10-23', 5.00, 3000)`,
+            `INSERT INTO ORDERS VALUES (8001, DATE '2016-03-18', 8.00, 3001)`,
+            `INSERT INTO ORDERS VALUES (8002, DATE '2017-01-19', 50.00, 3002)`,
+            `INSERT INTO ORDERS VALUES (8003, DATE '2018-08-21', 99.00, 3003)`,
+            `INSERT INTO ORDERS VALUES (8004, DATE '2019-11-22', 15.00, 3004)`,
+          
             `INSERT INTO PickupOrder VALUES (9000, 8000, 2000)`,
             `INSERT INTO PickupOrder VALUES (9001, 8002, 2001)`,
             `INSERT INTO PickupOrder VALUES (9002, 8003, 2002)`,
             `INSERT INTO PickupOrder VALUES (9003, 8004, 2003)`,
+          
             `INSERT INTO PackageOrder VALUES (10002, 8001, 7001)`,
             `INSERT INTO PackageOrder VALUES (10003, 8004, 7002)`
-        ];
+          ];
 
         for (const statement of insertStatements) {
             await connection.execute(statement, [], { autoCommit: true });
         }
 
-        res.status(200).send("Tables Populated successfully");
+        res.status(200);
 
     } catch (err) {
         console.error(err);
@@ -238,7 +252,7 @@ app.post('/drop-table', async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).send("Error dropping tables: " + err.message);
+        res.status(500);
     } finally {
         if (connection) {
             try {
@@ -251,8 +265,7 @@ app.post('/drop-table', async (req, res) => {
 });
 
 // Query Table 
-
-app.post('/drop-table', async (req, res) => {
+app.post('/query-table', async (req, res) => {
     let connection;
     try {
         connection = await oracledb.getConnection({
