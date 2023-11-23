6293c986bfaf79a252d23192c74aa58700cd0c5a
farah Adi <farahadi32@gmail.com>
2023-11-22 20:30:36 -0500

    added buttons

--- client/js/queryTable.js
+++ client/js/queryTable.js
@@ -1,10 +1,70 @@
-async function myFunction() {
-    const response = await fetch(`http://localhost:4000/query-table`);
-    
+async function myFunction1() {
+    const response = await fetch(`http://localhost:4000/query1-table`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+async function myFunction2() {
+
+    const response = await fetch(`http://localhost:4000/query2-table`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+async function myFunction3() {
+    const response = await fetch(`http://localhost:4000/query3-table`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+async function myFunction4() {
+    const response = await fetch(`http://localhost:4000/query4-table`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
